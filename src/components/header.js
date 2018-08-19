9f85c029d475e8410e6734277139f533dcdc1ed3
Guillermo Casanova <guillerizo@hotmail.com>
2018-08-20 00:53:09 +0100

    Setup i18n

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,43 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { I18n } from 'react-i18next'
+
+// Import a pre-configured instance of i18next
+import i18n from '../i18n'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+  <I18n>
+    {
+      (t, { i18n }) => (
+        <div
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            background: 'rebeccapurple',
+            marginBottom: '1.45rem',
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 960,
+              padding: '1.45rem 1.0875rem',
+            }}
+          >
+            <h1 style={{ margin: 0 }}>
+              <Link
+                to="/"
+                style={{
+                  color: 'white',
+                  textDecoration: 'none',
+                }}
+              >
+                {t('contactUs')}
+              </Link>
+            </h1>
+          </div>
+        </div>
+      )
+    }
+  </I18n>
 )
 
 export default Header
---
+++ src/i18n.js
@@ -0,0 +1,39 @@
+import i18n from 'i18next';
+import XHR from 'i18next-xhr-backend';
+import { reactI18nextModule } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+    .use(XHR)
+    .use(LanguageDetector)
+    .use(reactI18nextModule) // if not using I18nextProvider
+    .init({
+        debug: true,
+        load: 'languageOnly',
+        fallbackLng: 'es',
+        backend: {
+            loadPath: '/static/locales/{{lng}}/{{ns}}.json'
+        },
+
+        interpolation: {
+            escapeValue: false, // not needed for react!!
+        },
+
+        // react i18next special options (optional)
+        react: {
+            wait: false,
+            bindI18n: 'languageChanged loaded',
+            bindStore: 'added removed',
+            nsMode: 'default'
+        }
+    }, (err, t) => {
+        if (err) {
+            console.log('Something went wrong loading language', err);
+            return;
+        }
+
+        i18n.emit('init');
+    });
+
+
+export default i18n;
