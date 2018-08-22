73a9143d6576c838fae832e9a19118445580afbd
Guillermo Casanova <guillerizo@hotmail.com>
2018-08-22 13:23:02 +0100

    Lets not do translations for now, not required atm and slows down the showing of the text

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,46 +1,35 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import { I18n } from 'react-i18next';
 import { Navbar, Nav, NavItem, Image } from 'react-bootstrap';
 
 import './navbar.css';
-// import tizkanImage from '../../images/tizkan.png';
-
-// Import a pre-configured instance of i18next
-import i18n from '../../i18n';
 
 const myNavbar = () => (
-  <I18n wait={true}>
-    {
-      (t, { i18n }) => (
-        <Navbar ref='navbar' fixedTop>
-          <Navbar.Header>
-            {/* <Image src={tizkanImage} responsive /> */}
-          </Navbar.Header>
+  <Navbar ref='navbar' fixedTop>
+    <Navbar.Header>
+      {/* <Image src={tizkanImage} responsive /> */}
+    </Navbar.Header>
 
-          <Navbar.Collapse>
-            <Nav pullRight>
-              <NavItem>
-                <Link to="/">{t('navbar.inicio')}</Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/laser">{t('navbar.laser')}</Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/conocenos">{t('navbar.conocenos')}</Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/precios">{t('navbar.precios')}</Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/contacto">{t('navbar.contacto')}</Link>
-              </NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      )
-    }
-  </I18n>
+    <Navbar.Collapse>
+      <Nav pullRight>
+        <NavItem>
+          <Link to="/">INICIO</Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/laser">LASER</Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/conocenos">CONOCENOS</Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/precios">PRECIOS</Link>
+        </NavItem>
+        <NavItem>
+          <Link to="/contacto">CONTACTO</Link>
+        </NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 )
 
 export default myNavbar;
