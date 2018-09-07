e6584cd4730710bcca2ab7d754ee89c663cd4182
Guillermo Casanova <guillerizo@hotmail.com>
2018-09-08 00:17:43 +0100

    Add facebook logo

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,31 +5,61 @@ import { Navbar, Nav, NavItem, Image } from 'react-bootstrap';
 import './navbar.css';
 
 import logoImage from '../../../public/images/navbar-logo.png'
+import facebookLogoImage from '../../../public/images/facebook-logo.svg'
+import facebookLogoImageBlack from '../../../public/images/facebook-logo-black.svg'
 
-const myNavbar = () => (
-  <Navbar fixedTop collapseOnSelect>
-    <Navbar.Header>
-      <Link to="/"><Image width={180} src={logoImage} /></Link>
-      <Navbar.Toggle />
-    </Navbar.Header>
+class myNavbar extends React.Component {
+  constructor(props) {
+    super(props);
 
-    <Navbar.Collapse>
-      <Nav pullRight>
-        <NavItem>
-          <Link to="/" exact>INICIO</Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/laser" exact>LASER</Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/precios" exact>PRECIOS</Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/contacto" exact>CONTACTO</Link>
-        </NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-)
+    this.state = {
+      hovering: false
+    };
+  }
+
+  render() {
+    const facebookLogoStyle = {
+      display: 'inline-block',
+      width: '32px',
+      height: '32px',
+      'margin-left': '40px',
+      'margin-top': '7px',
+      backgroundImage: `url(${this.state.hovering ?
+        facebookLogoImage :
+        facebookLogoImageBlack})`
+    };
+
+    const handleMouseEnter = () => this.setState({ hovering: true });
+    const handleMouseLeave = () => this.setState({ hovering: false });
+
+    return (
+      <Navbar fixedTop collapseOnSelect>
+        <Navbar.Header>
+          <Link to='/'><Image width={180} src={logoImage} /></Link>
+          <Navbar.Toggle />
+        </Navbar.Header>
+
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem>
+              <Link to='/' exact>INICIO</Link>
+            </NavItem>
+            <NavItem>
+              <Link to='/laser' exact>LASER</Link>
+            </NavItem>
+            <NavItem>
+              <Link to='/precios' exact>PRECIOS</Link>
+            </NavItem>
+            <NavItem>
+              <Link to='/contacto' exact>CONTACTO</Link>
+            </NavItem>
+            <a href='https://www.facebook.com' onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave} style={facebookLogoStyle} target='_blank' />
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
+}
 
 export default myNavbar;
